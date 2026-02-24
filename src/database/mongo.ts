7614f2036dc4aa94f1b953502686661cb3a7a73d
Div <b510336@gmail.com>
2026-02-25 02:15:36 +0800

    feat: add MongoDB connection

---
+++ src/database/mongo.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const connectMongoDB = async () => {
+  try {
+    console.log(process.env.MONGO_URI);
+
+    const connection = await mongoose.connect(process.env.MONGO_URI!, {
+      user: process.env.MONGO_USER!,
+      pass: process.env.MONGO_PASSWORD!,
+      authSource: 'admin',
+    });
+
+    console.log(`MongoDB connected: ${connection.connection.host}:${connection.connection.port}`);
+  } catch (error) {
+    console.error('Failed to connect to MongoDB:', error);
+
+    process.exit(1);
+  }
+};
+
+export default connectMongoDB;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,22 @@
 import app from './app.js';
+import connectMongoDB from './database/mongo.js';
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  await Promise.all([
+    connectMongoDB(),
+  ]);
+
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
+};
+
+try {
+  await startServer();
+} catch (error) {
+  console.error('Unexpected error:', error);
+
+  process.exit(1);
+}
