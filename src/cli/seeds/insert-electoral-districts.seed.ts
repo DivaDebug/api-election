3b1a879cec20b04780288307a9c0f7a4181e218a
Div <b510336@gmail.com>
2026-03-03 01:37:32 +0800

    feat: refactor seed scripts to use runJob for execution

--- src/cli/seeds/insert-electoral-districts.seed.ts
+++ src/cli/seeds/insert-electoral-districts.seed.ts
@@ -1,24 +1,14 @@
-import {connectMongoDB, disconnectMongoDB} from '../../database/mongo.js';
 import {ElectoralDistrict} from '../../models/electoral-district.model.js';
 import fs from 'fs';
 import path from 'path';
+import runJob from '../run.js';
 
-async function handle(): Promise<void> {
+const handle = async (): Promise<void> => {
   const filePath = path.join(process.cwd(), 'src/data/2022-mayor-electoral-district-candidates.json');
 
   const electoralDistrictCandidates = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
 
   await ElectoralDistrict.insertMany(electoralDistrictCandidates);
-}
+};
 
-try {
-  await connectMongoDB();
-
-  await handle();
-} catch (error) {
-  console.error(error);
-
-  process.exit(1);
-} finally {
-  await disconnectMongoDB();
-}
+await runJob(handle);
