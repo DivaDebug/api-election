9d628493196a6659de76dfa2de49e2dc1350e7bb
Div <b510336@gmail.com>
2026-03-04 16:47:59 +0800

    feat: wrap transaction when create votes and update summary votes data

--- src/controllers/vote.controller.ts
+++ src/controllers/vote.controller.ts
@@ -1,17 +1,24 @@
 import {Request, Response} from 'express';
 import {voteService} from '../services/vote.service.js';
 import {votesSummaryService} from '../services/votes-summary.service.js';
+import mongoose from 'mongoose';
 
 const createVote = async (req: Request, res: Response) => {
   const params = req.body;
   const {year, type, townCode} = params.electoralDistrict;
 
-  const vote = await voteService.createVote(params);
+  try {
+    await mongoose.connection.transaction(async () => {
+      const vote = await voteService.createVote(params);
 
-  const countyCode = vote.electoralDistrict.countyCode;
+      const countyCode = vote.electoralDistrict.countyCode;
 
-  await votesSummaryService.recalculateTownVotesSummary({year, type, townCode});
-  await votesSummaryService.recalculateCountyVotesSummary({year, type, countyCode});
+      await votesSummaryService.recalculateTownVotesSummary({year, type, townCode});
+      await votesSummaryService.recalculateCountyVotesSummary({year, type, countyCode});
+    });
+  } catch (error) {
+    throw error;
+  }
 
   res.json({
     meta: {
