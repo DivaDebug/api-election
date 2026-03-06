8ca0dfd726f9b8efb5518fa4eaabcd33c39b28fa
Div <b510336@gmail.com>
2026-03-06 11:57:58 +0800

    feat: add schema validation before createVote

--- src/routes/admin/vote.route.ts
+++ src/routes/admin/vote.route.ts
@@ -1,5 +1,7 @@
 import {Router} from 'express';
+import validate from '../../middlewares/validate.middleware.js';
 import {createVote} from '../../controllers/admin/vote.controller.js';
+import createVoteSchema from '../../schemas/create-vote.schema.js';
 
 const router = Router();
 
@@ -7,6 +9,6 @@ router.get('', (req, res) => {
   res.json({path: '/vote'});
 });
 
-router.post('/', createVote);
+router.post('/', validate({body: createVoteSchema}), createVote);
 
 export default router;
---
+++ src/schemas/create-vote.schema.ts
@@ -0,0 +1,15 @@
+import z from 'zod';
+
+const createVoteSchema = z.object({
+  electoralDistrict: z.object({
+    year: z.number(),
+    type: z.enum(['mayor']),
+    townCode: z.string(),
+    candidate: z.object({
+      no: z.number(),
+    }),
+  }),
+  votes: z.number(),
+});
+
+export default createVoteSchema;
