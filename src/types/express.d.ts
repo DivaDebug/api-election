72c122aad71dbef9abea889b9c8d2e3c0c4aee23
Div <b510336@gmail.com>
2026-03-08 22:19:55 +0800

    feat: extend Express types with validated Request interface and fix extend type success return type with 'this' in interface Response

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,9 +1,21 @@
-declare namespace Express {
-  interface Response {
-    success<T = unknown>(
-      data?: T | null,
-      meta?: Record<string, unknown>,
-      statusCode?: number,
-    ): Response;
+declare global {
+  namespace Express {
+    interface Request {
+      validated?: {
+        params?: unknown;
+        query?: unknown;
+        body?: unknown;
+      };
+    }
+
+    interface Response {
+      success<T = unknown>(
+        data?: T | null,
+        meta?: Record<string, unknown>,
+        statusCode?: number,
+      ): this;
+    }
   }
 }
+
+export {};
