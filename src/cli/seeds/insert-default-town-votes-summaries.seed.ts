e669c655e4eb9f2cbf9e21e7c1918880b7dd268a
Div <b510336@gmail.com>
2026-03-14 02:41:57 +0800

    fix: can not find json data when execute seeds script

--- src/cli/seeds/insert-default-town-votes-summaries.seed.ts
+++ src/cli/seeds/insert-default-town-votes-summaries.seed.ts
@@ -1,10 +1,14 @@
 import {TownVotesSummary} from '../../models/town-votes-summary.model.js';
 import fs from 'fs';
 import path from 'path';
+import {fileURLToPath} from 'url';
 import runJob from '../run.js';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const dataDir = path.join(__dirname, '../../data');
+
 const handle = async (): Promise<void> => {
-  const filePath = path.join(process.cwd(), 'src/data/2022-mayor-default-town-votes-summaries.json');
+  const filePath = path.join(dataDir, '2022-mayor-default-town-votes-summaries.json');
 
   const townVotesSummaries = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
 
--- src/cli/seeds/insert-electoral-districts.seed.ts
+++ src/cli/seeds/insert-electoral-districts.seed.ts
@@ -1,10 +1,14 @@
 import {ElectoralDistrict} from '../../models/electoral-district.model.js';
 import fs from 'fs';
 import path from 'path';
+import {fileURLToPath} from 'url';
 import runJob from '../run.js';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const dataDir = path.join(__dirname, '../../data');
+
 const handle = async (): Promise<void> => {
-  const filePath = path.join(process.cwd(), 'src/data/2022-mayor-electoral-district-candidates.json');
+  const filePath = path.join(dataDir, '2022-mayor-electoral-district-candidates.json');
 
   const electoralDistrictCandidates = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
 
