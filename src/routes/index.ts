6be38df4b5b51cbfd441abc24e300b076e2c80ca
Div <b510336@gmail.com>
2026-03-15 22:21:48 +0800

    feat: add health check API

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,13 @@
-import {Router} from 'express';
+import {Router, Request, Response} from 'express';
 import adminRoute from './admin/admin.route.js';
 import countyVotesSummaryRoute from './county-votes-summary.route.js';
 import townVotesSummaryRoute from './town-votes-summary.route.js';
 
 const router = Router();
+
+router.get('/health', (req: Request, res: Response) => {
+  res.success();
+});
 
 router.use('/admin', adminRoute);
 router.use('/county-votes-summaries', countyVotesSummaryRoute);
