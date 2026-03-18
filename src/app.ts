b7674107f6a12aea77007cd67e5b4fe8cff7f5f3
Div <b510336@gmail.com>
2026-03-18 14:56:20 +0800

    feat: apply cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import routes from './routes/index.js';
+import cors from 'cors';
 import routeNotFoundMiddleware from './middlewares/route-not-found.middleware.js';
 import exceptionHandlerMiddleware from './middlewares/exception-handler.middleware.js';
 import responseMiddleware from './middlewares/response.middleware.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(responseMiddleware);
 
